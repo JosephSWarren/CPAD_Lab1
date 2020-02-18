3725ba696c9aa13021316979d12016768d94fe48
JosephSWarren <jswarren@miners.utep.edu>
2020-02-17 22:35:18 -0700

    First iteration of sessionQuiz constructor

--- lib/SessionQuiz.dart
+++ lib/SessionQuiz.dart
@@ -4,6 +4,7 @@ import 'dart:math';
 class SessionQuiz{
   List<int> question; //Index of all questions that will be used for quiz
   int _grade;
+  int awardedPoints;
   int currentQuestion;
 
   sessionQuiz(int maxQuestions, [int numQuestions =5]){
@@ -11,9 +12,14 @@ class SessionQuiz{
       numQuestions=maxQuestions;
     }
     var rng = new Random();
+    int randomNumber;
     for(int i=0; i<numQuestions; i++){
-      question.append
-
+      while(question.contains(randomNumber=rng.nextInt(maxQuestions))){ //Probably a terrible way to do this.
+        question.add(randomNumber);
+      }
     }
+    currentQuestion=awardedPoints=0;
   }
+
+  
 }
